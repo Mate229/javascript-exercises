92c796669d46a61f432799ab60d1570c94824c96
Mate229 <adiokhalidsalami@gmail.com>
2025-08-15 05:01:14 +0100

    do exo

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  const celsius = (fah - 32) * 5 / 9;
+  return Number(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  const fah = (cel * 9 / 5) + 32;
+  return Number(fah.toFixed(1));
 };
 
 // Do not edit below this line
