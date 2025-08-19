3fa030855aee85acb59ca0ccab922ef80a3799c1
Mate229 <adiokhalidsalami@gmail.com>
2025-08-19 20:20:44 +0100

    do exercice

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return +a + +b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return +a - +b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	return a.reduce((total, item) => total + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let produit = 1;
+  for(let i = 0; i < a.length; i++) {
+    produit *= a[i];
+  }
+  return produit;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) {
+    return 1;
+  } else {
+    let facteur = 1;
+    for(let i = 1; i <= a; i++) {
+      facteur *= i;
+    }
+    return facteur;
+  }
 };
 
 // Do not edit below this line
