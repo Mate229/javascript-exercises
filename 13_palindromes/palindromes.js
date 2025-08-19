1417b66d538cacaae41cfd72e833dc1c8cb41a07
Mate229 <adiokhalidsalami@gmail.com>
2025-08-19 21:37:08 +0100

    do exo

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    let b = a.split("").reverse().join("");
+    const remove = function (str) {
+        return str.replace(/[\s!,.\\d+]/g, "");
+    }
+    if (remove(a).toLowerCase() === remove(b).toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    };
 };
 
 // Do not edit below this line
