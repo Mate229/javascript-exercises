156775fc76833a9d7c76d1b0337939a14dcef966
Mate229 <adiokhalidsalami@gmail.com>
2025-08-19 23:22:41 +0100

    do exo

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(a) {
+    for(let i = 0; i < a.length; i++) {
+        if ("yearOfDeath" in a[i] == false) {
+            a[i]["yearOfDeath"] = (new Date).getFullYear();
+        };
+    };
+    let b = a.sort((c, d) => (d.yearOfDeath - d.yearOfBirth) - (c.yearOfDeath - c.yearOfBirth));
+    return b[0];
 };
 
 // Do not edit below this line
