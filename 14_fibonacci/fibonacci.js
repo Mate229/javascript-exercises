ac60e729153c51f8611d61a29ef9a18eab364e6f
Mate229 <adiokhalidsalami@gmail.com>
2025-08-19 22:18:26 +0100

    do exo

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if (parseInt(a) < 0) {
+        return "OOPS";
+    } else {
+        if (parseInt(a) < 2) {
+            return parseInt(a);
+        } else {
+            return fibonacci(parseInt(a) - 1) + fibonacci (parseInt(a) -2);
+        }
+    }
 };
 
 // Do not edit below this line
